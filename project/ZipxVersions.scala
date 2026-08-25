59e4ea86cfb535015e93f070fa6a266d4089052e
Russ White <356303+russwyte@users.noreply.github.com>
2026-08-25 18:19:54 -0500

    Bump sbt-zipx to 0.8.1 and sbt to 2.0.7

--- project/ZipxVersions.scala
+++ project/ZipxVersions.scala
@@ -5,7 +5,7 @@ import zipx.*
   * is not a row: zipx already brings it in.
   */
 object MyVersions extends ZipxVersions:
-  val sbt: SbtVersion     = SbtVersion("2.0.6")
+  val sbt: SbtVersion     = SbtVersion("2.0.7")
   val scala: ScalaVersion = ScalaVersion("3.8.4")
 
   val zio        = Lib("dev.zio", "zio", "2.1.26")
