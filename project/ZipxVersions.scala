1e67cbd83e10753bb01daa5aa221cda89d26ab19
Russ White <356303+russwyte@users.noreply.github.com>
2026-09-13 17:53:16 -0500

    Bump Scala to 3.9.0 and zio-json to 1.1.0
    Recompile protocol codecs against zio-json's Scala 3 macros so consumers
    that take heddle's 1.1.0 no longer miss Magnolia or JsonEncoderDerivation.
    
    ThisBuild / scalaVersion matches the catalog so zipxCheckDeps agrees.

--- project/ZipxVersions.scala
+++ project/ZipxVersions.scala
@@ -6,11 +6,11 @@ import zipx.*
   */
 object MyVersions extends ZipxVersions:
   val sbt: SbtVersion     = SbtVersion("2.0.7")
-  val scala: ScalaVersion = ScalaVersion("3.8.4")
+  val scala: ScalaVersion = ScalaVersion("3.9.0")
 
   val zio        = Lib("dev.zio", "zio", "2.1.26")
   val zioStreams = zio.mod("zio-streams")
-  val zioJson    = Lib("dev.zio", "zio-json", "0.10.0")
+  val zioJson    = Lib("dev.zio", "zio-json", "1.1.0")
   val zioTest    = zio.mod("zio-test")
   val zioTestSbt = zio.mod("zio-test-sbt")
 
