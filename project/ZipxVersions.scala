a6bc0e794ba6f663e48c693acd3e21994c8b54e5
Russ White <356303+russwyte@users.noreply.github.com>
2026-09-13 21:46:43 -0500

    Bump specular to 0.16.0 and sbt to 2.1.0-M1
    sbt-specular 0.16.0 is TASTy 28.9, so the metabuild moves to 2.1.0-M1.
    ascent-js 0.7.0 and sbt-zipx 0.11.0 follow the same stack.

--- project/ZipxVersions.scala
+++ project/ZipxVersions.scala
@@ -5,7 +5,7 @@ import zipx.*
   * is not a row: zipx already brings it in.
   */
 object MyVersions extends ZipxVersions:
-  val sbt: SbtVersion     = SbtVersion("2.0.7")
+  val sbt: SbtVersion     = SbtVersion("2.1.0-M1")
   val scala: ScalaVersion = ScalaVersion("3.9.0")
 
   val zio        = Lib("dev.zio", "zio", "2.1.26")
@@ -16,16 +16,16 @@ object MyVersions extends ZipxVersions:
 
   val scalajsDom    = Lib("org.scala-js", "scalajs-dom", "2.8.0")
   val scalajsJsEnvs = Lib("org.scala-js", "scalajs-js-envs", "1.6.0")
-  val ascentJs      = Lib("rocks.earlyeffect", "ascent-js", "0.3.1")
+  val ascentJs      = Lib("rocks.earlyeffect", "ascent-js", "0.7.0")
 
-  val specular        = Lib("rocks.earlyeffect", "specular-core", "0.14.1")
+  val specular        = Lib("rocks.earlyeffect", "specular-core", "0.16.0")
   val specularZioTest = specular.mod("specular-zio-test").test
   val specularTheme   = specular.mod("early-effect-docs-theme").test
 
   val scalajs        = Plugin("org.scala-js", "sbt-scalajs", "1.22.0")
   val scalafmt       = Plugin("org.scalameta", "sbt-scalafmt", "2.6.2")
   val dynverCi       = Plugin("rocks.earlyeffect", "sbt-dynver-ci", "0.2.3")
-  val specularPlugin = Plugin("rocks.earlyeffect", "sbt-specular", "0.14.1")
+  val specularPlugin = Plugin("rocks.earlyeffect", "sbt-specular", "0.16.0")
   val sbtSplice      = Plugin("rocks.earlyeffect", "sbt-splice", "0.1.0")
 
   def zioCore     = library(zio, zioStreams)
